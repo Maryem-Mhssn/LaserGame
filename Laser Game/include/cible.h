96125549846e264dfec6145c80b40f32df12e06e
edonadbr <edona55@hotmail.fr>
2022-01-05 00:15:52 +0100

    documentation laser et cible

--- Laser Game/include/cible.h
+++ Laser Game/include/cible.h
@@ -1,13 +1,47 @@
+/**
+ * @headerfile cible.h
+ * @author
+ * @brief Fichier header contenant la classe cible
+ */
 #ifndef CIBLE_H
 #define CIBLE_H
 #include "box.h"
 
+/**
+ * @class cible derivée de box
+ */
 class cible : public box
 {
 public:
+    
+    /**
+     * @fn cible ()
+     * @brief Constructeur
+     *
+     */
     cible();
+    
+    /**
+        @fn cible(int x, int y, char c)
+        @brief Constructeur
+        @param x position x de la cible
+        @param y position y de la cible
+        @param c caractere x de la cible
+    */
     cible(int x, int y, char c);
+    
+    /**
+     * @fn void setTouched()
+     * @brief définit la cible comme étant touchée
+     */
     void setTouched();
+    
+    
+    /**
+     * @fn void touched()
+     * @brief renvoie si la cible a été touché ou non
+     * @return d_touched
+     */
     bool touched() const;
 
 private:
--- Laser Game/include/laser.h
+++ Laser Game/include/laser.h
@@ -1,3 +1,9 @@
+/**
+ * @headerfile laser.h
+ * @author
+ * @brief Fichier header contenant la classe laser
+ */
+
 #ifndef LASER_H
 #define LASER_H
 #include "box.h"
@@ -5,16 +11,52 @@
 #define DIM 20
 using std::vector;
 
+/**
+ @class laser derivée de box
+ */
 class laser : public box{
     static constexpr int degre = 90;
 
 public :
+    /**
+     * @fn laser ()
+     * @brief Constructeur
+     *
+     */
     laser();
+    
+    /**
+        @fn laser(int x, int y)
+        @brief Constructeur
+        @param x position x du laser
+        @param y position y du laser
+    */
     laser(int x,int y);
+    
+    
+    /**
+     @fn tire()
+     @brief le laser tire
+     @return boxes où les tires sont lancé
+     */
     vector<box> tire();
+    
+    
+    
+    /**
+     @fn changeTrajectoirDroite()
+     @brief change trajectoir vers la droite lors de la rencontre avec un mirroir
+     */
     void changeTrajectoirDroite();
+    
+    
+    
+    /**
+     @fn changeTrajectoirGauche()
+     @brief change trajectoir vers la gauche lors de la rencontre avec un mirroir
+     */
     void changeTrajectoirGauche();
 
 };
 
-#endif // LASER_H
+#endif
